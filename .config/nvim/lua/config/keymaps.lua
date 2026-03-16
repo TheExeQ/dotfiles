06280e0e8650af66caaa795cc5a12f711df83e5d
Samuel Ryberg <samuel.ryberg@gmail.com>
2026-03-16 18:00:12 +0100

    chore: removed old ideavimrc, nop mapped arrow keys in normal and visual mode for nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Disable arrow keys in all major modes
+local modes = { "n", "v", "x" }
+local keys = { "<Up>", "<Down>", "<Left>", "<Right>" }
+
+for _, mode in ipairs(modes) do
+  for _, key in ipairs(keys) do
+    vim.keymap.set(mode, key, "<Nop>", { noremap = true, silent = true })
+  end
+end
